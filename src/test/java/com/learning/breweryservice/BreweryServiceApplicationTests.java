e59c456c72810d1f84309be14fe4ecdcdfc79861
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-04-03 03:20:23 +0600

    initial commit

---
+++ src/test/java/com/learning/breweryservice/BreweryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.breweryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
